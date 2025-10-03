e805366d1031ac9c3f8adede8752e94bf9aeb160
jithesh79 <jitheshks11@gmail.com>
2025-10-03 10:52:17 +0530

    Create Public_subnet .tf

---
+++ Public_subnet .tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "public_sg" {
+  name        = "public-subnet-sg"
+  description = "Allow inbound HTTP/HTTPS from anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Public HTTP access
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Public HTTPS access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all outbound
+  }
+}
